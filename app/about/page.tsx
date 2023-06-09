eb13db6fbe3bf31a586d403ac39574483a2b274d
jbxamora <jbxamora@icloud.com>
2023-06-09 15:12:39 -0700

    update mtdata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ import React from "react";
 
 export const metadata: Metadata = {
   title: "About",
-  description: "VP of Developer Experience at Vercel.",
+  description: "Tattoo Apprentice at Lyfestyle Ink",
 };
 
 export default function AboutPage() {
